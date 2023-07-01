ab3f8930bb836a0f8d8774b642e4b662a05ec251
heeman <abolfazlsadeqi@gmail.com>
2023-07-01 20:46:55 +0430

    add Web Project and Add Report BusinessObject to StimulSoft

---
+++ Src/Base/Models/Dto/EmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Dto;
+
+public class EmployeeDto
+{
+
+
+    [Key]
+    public int Id { set; get; }
+    public string FirstName { set; get; }
+    public string LastName { set; get; }
+    public string Position { set; get; }
+
+
+}
---
+++ Src/UI/Web/Controllers/ReportsController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Models.Dto;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Mvc;
+using System.Security.Cryptography.Xml;
+
+namespace Web.Controllers;
+
+public class ReportsController : Controller
+{
+    private readonly IHostEnvironment _hostEnvironment;
+
+    public ReportsController( IHostEnvironment hostEnvironment)
+    {
+       
+        _hostEnvironment = hostEnvironment; // has ContentRootPath property
+
+    }
+
+
+    public IActionResult Index()
+    {
+        return View();
+
+    }
+
+    public IActionResult GetReport()
+    {
+
+        var report = new StiReport();
+        report.Load(_hostEnvironment.ContentRootPath + @"\Reports\ReportTest.mrt");
+        report.RegBusinessObject("Employee", GetAll());
+        return StiNetCoreViewer.GetReportResult(this, report);
+    }
+
+    List<EmployeeDto> GetAll()
+    {
+        return new List<EmployeeDto>() { new EmployeeDto() {  FirstName="test",LastName="bbb" , Position="Test"}
+       , new EmployeeDto() {  FirstName="test",LastName="bbb" , Position="Test"}};
+    }
+    public IActionResult ViewerEvent()
+    {
+        return StiNetCoreViewer.ViewerEventResult(this);
+    }
+}
